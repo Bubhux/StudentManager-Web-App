8f54a8f166a8f6ce987eb514a99a33deac435e4c
Bubhux <bubhuxpaindepice@gmail.com>
2025-04-28 21:41:02 +0200

    Mise à jour V.1.5  Changes to be committed: 	modified:   StudentManager/urls.py

--- StudentManager/urls.py
+++ StudentManager/urls.py
@@ -17,13 +17,27 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import home_view, student_management_view, classroom_management_view, quit_application_view
-
+from classroom.views import (
+    classroom_home_view, display_classrooms_view, add_classroom_view,
+    update_classroom_info_view, add_students_to_classroom_view,
+    delete_students_from_classroom_view, calculate_classroom_average_view,
+    delete_classroom_view
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
     path('', home_view, name='home'),
     path('student-management/', student_management_view, name='student_management'),
     path('classroom-management/', classroom_management_view, name='classroom_management'),
     path('quit/', quit_application_view, name='quit_application'),
+
+    # Routes pour l'application classroom
+    path('classroom/', classroom_home_view, name='classroom_home'),
+    path('classroom/display/', display_classrooms_view, name='display_classrooms'),
+    path('classroom/add/', add_classroom_view, name='add_classroom'),
+    path('classroom/update/', update_classroom_info_view, name='update_classroom_info'),
+    path('classroom/add-students/', add_students_to_classroom_view, name='add_students_to_classroom'),
+    path('classroom/delete-students/', delete_students_from_classroom_view, name='delete_students_from_classroom'),
+    path('classroom/calculate-average/', calculate_classroom_average_view, name='calculate_classroom_average'),
+    path('classroom/delete/', delete_classroom_view, name='delete_classroom'),
 ]
