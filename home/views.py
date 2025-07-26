367fbfecd01a7b5493be38355faa31d879a8e439
Bubhux <bubhuxpaindepice@gmail.com>
2025-07-26 14:23:40 +0200

    Mise à jour V.2.3  On branch development  Your branch is up to date with 'origin/development'.
     Changes to be committed:
    	modified:   home/views.py

--- home/views.py
+++ home/views.py
@@ -6,10 +6,10 @@ def home_view(request):
     return render(request, 'home/home.html')
 
 def student_management_view(request):
-    return render(request, '/student/')  # Redirige vers l'application student
+    return redirect('/student/')  # Redirige vers l'application student
 
 def classroom_management_view(request):
-    return render(request, '/classroom/')  # Redirige vers l'application classroom
+    return redirect('/classroom/')  # Redirige vers l'application classroom
 
 def quit_application_view(request):
-    return render(request, '/home/')
+    return redirect('/home/')
