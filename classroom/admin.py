4dec61fff43620c581e8ebdee1049c41897e2862
Bubhux <bubhuxpaindepice@gmail.com>
2025-09-27 14:56:56 +0200

    Mise à jour V.3.5  On branch development  Your branch is up to date with 'origin/development'.
     Changes to be committed:
    	modified:   classroom/admin.py

--- classroom/admin.py
+++ classroom/admin.py
@@ -1,3 +1,19 @@
+# classroom/admin.py
 from django.contrib import admin
+from .models import Classroom
+from django.utils.html import format_html
 
-# Register your models here.
+
+class ClassroomAdmin(admin.ModelAdmin):
+    list_display = ('classroom_name', 'number_of_places_available', 'student_count')
+    list_filter = ('classroom_name',)
+    search_fields = ('classroom_name',)
+
+    def student_count(self, obj):
+        return obj.students.count()
+
+    student_count.short_description = "Nombre d'étudiants"
+    student_count.admin_order_field = 'students__count'
+
+
+admin.site.register(Classroom, ClassroomAdmin)
