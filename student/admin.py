86eacd52e546a9373f12622e53da8bd87483dd19
Bubhux <bubhuxpaindepice@gmail.com>
2025-11-17 12:07:44 +0100

    Mise à jour V.4.7  On branch development  Your branch is up to date with 'origin/development'.
     Changes to be committed:
    	modified:   student/admin.py

--- student/admin.py
+++ student/admin.py
@@ -1,9 +1,31 @@
 # student/admin.py
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
 from .models import Student, Lesson, StudentLesson
 
+# Formulaire personnalisé pour la création d'un étudiant sans username/password obligatoire
+class StudentCreationForm(UserCreationForm):
+    class Meta:
+        model = Student
+        fields = ('first_name', 'last_name', 'email', 'classroom')
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].required = False
+        self.fields['password2'].required = False
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        # Si aucun mot de passe n'est fourni, définir un mot de passe par défaut (ou inutilisable)
+        if not user.password:
+            user.set_unusable_password()
+        if commit:
+            user.save()
+        return user
+
+# Inline pour les notes
 class StudentLessonInline(admin.TabularInline):
     model = StudentLesson
     extra = 1
@@ -11,42 +33,30 @@ class StudentLessonInline(admin.TabularInline):
     verbose_name = "Lesson associée"
     verbose_name_plural = "Lessons associées avec notes"
 
-
+# Admin personnalisé pour Student
 class StudentAdmin(UserAdmin):
-    list_display = ('username', 'first_name', 'last_name', 'classroom', 'email')
-    list_filter = ('classroom', 'is_staff', 'is_superuser')
-    search_fields = ('first_name', 'last_name', 'username', 'email')
-    filter_horizontal = ('groups', 'user_permissions')
-
-    fieldsets = (
-        (None, {'fields': ('username', 'password')}),
-        ('Informations personnelles', {'fields': ('first_name', 'last_name', 'email')}),
-        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Informations importantes', {'fields': ('classroom', 'last_login', 'date_joined')}),
-    )
-
+    add_form = StudentCreationForm
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'password1', 'password2', 'first_name', 'last_name', 'email', 'classroom'),
+            'fields': ('first_name', 'last_name', 'email', 'classroom'),
         }),
     )
-
+    list_display = ('email', 'first_name', 'last_name', 'classroom')
+    list_filter = ('classroom', 'is_staff', 'is_superuser')
+    search_fields = ('first_name', 'last_name', 'email')
+    ordering = ('email',)
+    
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Informations personnelles', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Informations importantes', {'fields': ('classroom', 'last_login', 'date_joined')}),
+    )
+    
     inlines = [StudentLessonInline]
 
-
-@admin.register(Lesson)
-class LessonAdmin(admin.ModelAdmin):
-    list_display = ('name',)
-    search_fields = ('name',)
-
-
-@admin.register(StudentLesson)
-class StudentLessonAdmin(admin.ModelAdmin):
-    list_display = ('student', 'lesson', 'grade')
-    list_filter = ('student__classroom', 'lesson')
-    search_fields = ('student__first_name', 'student__last_name', 'lesson__name')
-    raw_id_fields = ('student', 'lesson')
-
-
+# Enregistrement des modèles
 admin.site.register(Student, StudentAdmin)
+admin.site.register(Lesson)
+admin.site.register(StudentLesson)
